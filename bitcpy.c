6c65b85d8f1a1afc1da55190cc55ffd25867b125
Steven HH Chen <s335276@gmail.com>
2020-03-28 01:07:56 +0800

    Add code description and answering question in quiz 4

--- bitcpy.c
+++ bitcpy.c
@@ -1,5 +1,5 @@
-#define KK1 0x0
-#define KK2 0x0
+#define KK1 7
+#define KK2 bitsize
 
 #include <stdint.h>
 #include <stdio.h>
@@ -11,15 +11,20 @@ void bitcpy(void *_dest,      /* Address of the buffer to write to */
             size_t _read,     /* Bit offset to start reading from */
             size_t _count)
 {
+
+    /* Local variable initialization */
     uint8_t data, original, mask;
     size_t bitsize;
-    size_t read_lhs = _read & 7;
-    size_t read_rhs = 8 - read_lhs;
-    const uint8_t *source = _src + (_read / 8);
-    size_t write_lhs = _write & KK1;
-    size_t write_rhs = 8 - write_lhs;
-    uint8_t *dest = _dest + (_write / 8);
 
+    size_t read_lhs = _read & 7;                // Perform _read mod 8 in bitwise operation
+    size_t read_rhs = 8 - read_lhs;             // Get bit position offset in a uint8_t space
+    const uint8_t *source = _src + (_read / 8); // Offset to assigned array position for reading src
+
+    size_t write_lhs = _write & KK1;            // Perform _write mod 8 in bitwise operation
+    size_t write_rhs = 8 - write_lhs;           // Get bit position offset in a uint8_t space
+    uint8_t *dest = _dest + (_write / 8);       // Offset to assigned array position for writing dest
+
+    /* Mask for reading bits except leading 0~8 bits */
     static const uint8_t read_mask[] = {
         0x00, /*	== 0	00000000b	*/
         0x80, /*	== 1	10000000b	*/
@@ -32,6 +37,7 @@ void bitcpy(void *_dest,      /* Address of the buffer to write to */
         0xFF  /*	== 8	11111111b	*/
     };
 
+    /* Mask for writing bits except tailing 0~8 bits */
     static const uint8_t write_mask[] = {
         0xFF, /*	== 0	11111111b	*/
         0x7F, /*	== 1	01111111b	*/
@@ -44,37 +50,63 @@ void bitcpy(void *_dest,      /* Address of the buffer to write to */
         0x00  /*	== 8	00000000b	*/
     };
 
+    /* Copy all bits in number _count */
     while (_count > 0) {
+
+        // Read data from source and move to next position
         data = *source++;
-        bitsize = (_count > 8) ? 8 : _count;
+        bitsize = (_count > 8) ? 8 : _count;   // Set Operating bit size
+
+        // Bit offset in uint8_t space is not zero
         if (read_lhs > 0) {
-            data <<= read_lhs;
-            if (bitsize > read_rhs)
-                data |= (*source >> read_rhs);
+            data <<= read_lhs;        // Align to position zero
+            if (bitsize > read_rhs)   // If tailed reading bits in the next position of array
+                data |= (*source >> read_rhs);    // Concate these bit into current position of array
         }
 
+        // Reading bit size is smaller than 8, mask bits which are not necessary
         if (bitsize < 8)
             data &= read_mask[bitsize];
 
+        // Assign data before writing to variable original
         original = *dest;
+
+        // Bit offset in uint8_t space is not zero
         if (write_lhs > 0) {
+
+            // Assign read mask for leading 0~8 bits
             mask = read_mask[write_lhs];
+
+            // Writing bits is crossing two elements in array
             if (bitsize > write_rhs) {
+                // Mask bits in front of start position, and write available bits started from offset
                 *dest++ = (original & mask) | (data >> write_lhs);
+
+                // Write bits which cannot write into previous position
                 original = *dest & write_mask[bitsize - write_rhs];
+
+                // Move written bits to correct position
                 *dest = original | (data << write_rhs);
             } else {
+
+                // For writing position is not started from zero case, adjust mask for writing bits
                 if ((bitsize - write_lhs) > 0)
                     mask = mask | write_mask[8 - (bitsize - write_lhs)];
+
+                // Assign data with mask and concate data if necessary
                 *dest++ = (original & mask) | (data >> write_lhs);
             }
         } else {
+
+            // Mask data output while uint8_t is not fully written
             if (bitsize < 8)
                 data = data | (original & write_mask[bitsize]);
+
+            // Data assignment
             *dest++ = data;
         }
 
-        _count -= KK2;
+        _count -= KK2; // Write bitsize bits for each iteration, minus bitsize
     }
 }
 
@@ -87,6 +119,8 @@ static inline void dump_8bits(uint8_t _data)
 {
     for (int i = 0; i < 8; ++i)
         printf("%d", (_data & (0x80 >> i)) ? 1 : 0);
+
+    printf(" ");
 }
 static inline void dump_binary(uint8_t *_buffer, size_t _length)
 {
@@ -96,13 +130,19 @@ static inline void dump_binary(uint8_t *_buffer, size_t _length)
 
 int main(int _argc, char **_argv)
 {
-    memset(&input[0], 0xFF, sizeof(input));
+    /* Set all input to 0xFF */
+    memset(&input[0], 0xAF, sizeof(input));
 
+    printf("Copied bits count(i), input bit offset(j), output bit offset(k)\n");
+
+    /* Testbench for bitcpy */
     for (int i = 1; i <= 32; ++i) {
         for (int j = 0; j < 16; ++j) {
             for (int k = 0; k < 16; ++k) {
                 memset(&output[0], 0x00, sizeof(output));
-                printf("%2d:%2d:%2d ", i, k, j);
+                printf("%2d:%2d:%2d ", i, j, k);
+
+                /* Copy i bits from start of input + j bit offset to start of output + k bit offset */
                 bitcpy(&output[0], k, &input[0], j, i);
                 dump_binary(&output[0], 8);
                 printf("\n");
